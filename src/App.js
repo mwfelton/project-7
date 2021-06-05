1cbe983749a14b594b27289b39bf93570f28d6d4
mwfelton <72751046+mwfelton@users.noreply.github.com>
2021-06-05 14:14:11 +0100

    work on search form
    I'm stuck with the form throwing the arrow. you'll see commented out is the componentdidmount. this url did return the results however in my search query it does not

--- src/App.js
+++ src/App.js
@@ -1,60 +1,111 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
-import apiKey from '../src/config';
+import apiKey from '../src/Config';
 import './App.css';
+import PhotoContainer from './components/PhotoContainer.js'
+import Nav from './components/Nav.js'
+import SearchForm from './components/SearchForm.js'
 
-import PhotoContainer from '../src/components/PhotoContainer'
 
+class App extends Component {
+  state = {
+    photos: []
+  }
 
-function App() {
-  return (
-    <div className="container">
-      
-      <form className="search-form">
-        <input type="search" name="search" placeholder="Search" required/>
-        <button type="submit" class="search-button">
-          <svg fill="#fff" height="24" viewBox="0 0 23 23" width="24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
-            <path d="M0 0h24v24H0z" fill="none"/>
-          </svg>
-        </button>
-      </form>
+  // componentDidMount() {
+  //       axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=kailash&safe_search=&per_page=24&format=json&nojsoncallback=1`)
+  //       .then(response => {
+  //         this.setState({
+  //           photos: response.data.photos.photo
+  //         })
+  //       })
+  //       .catch( error => {
+  //         console.log('error fetching and parsing')
+  //       })
+  // }
 
-      <nav className="main-nav">
-        <ul>
-          <li>Cats</li>
-          {/* <li><a href='#'>Dogs</a></li>
-          <li><a href='#'>Computers</a></li> */}
-        </ul>
-      </nav>
+  performSearch(query) {
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=%22${query}%22&per_page=24&format=json&nojsoncallback=1`)
+        .then(response => {
+          this.setState({
+            photos: response.data.photos.photo
+          })
+        })
+        .catch( error => {
+          console.log('error fetching and parsing')
+        })
+  }
 
-      <PhotoContainer />
+  render() {
+    console.log(this.state.photos)
+    return (
+      <div className="container">
 
-      {/* <div class="photo-container">
-        <h2>Results</h2>
-        <ul>
-          <li>
-            <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" />
-          </li>
-          <li>
-            <img src="https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg" alt="" />
-          </li>
-          <li>
-            <img src="https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg" alt="" />
-          </li>
-          <li>
-            <img src="https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg" alt="" />
-          </li>
-          
-          <li class="not-found">
-            <h3>No Results Found</h3>
-            <p>You search did not return any results. Please try again.</p>
-          </li>
-        </ul>
-      </div> */}
+        <SearchForm onSearch={this.performSearch}/>
+        <Nav />
+        <PhotoContainer data={this.state.photos} />
 
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
 export default App;
+
+
+// function App() {
+//   return (
+//     <Container 
+
+//       <SearchForm />
+//       <Nav />
+//       <PhotoContainer />
+//     />
+      
+//       <form className="search-form">
+//         <input type="search" name="search" placeholder="Search" required/>
+//         <button type="submit" class="search-button">
+//           <svg fill="#fff" height="24" viewBox="0 0 23 23" width="24" xmlns="http://www.w3.org/2000/svg">
+//             <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+//             <path d="M0 0h24v24H0z" fill="none"/>
+//           </svg>
+//         </button>
+//       </form>
+
+//       <nav className="main-nav">
+//         <ul>
+//           <li>Cats</li>
+//           {/* <li><a href='#'>Dogs</a></li>
+//           <li><a href='#'>Computers</a></li> */}
+//         </ul>
+//       </nav>
+
+//       <PhotoContainer />
+
+//       {/* <div class="photo-container">
+//         <h2>Results</h2>
+//         <ul>
+//           <li>
+//             <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" />
+//           </li>
+//           <li>
+//             <img src="https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg" alt="" />
+//           </li>
+//           <li>
+//             <img src="https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg" alt="" />
+//           </li>
+//           <li>
+//             <img src="https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg" alt="" />
+//           </li>
+          
+//           <li class="not-found">
+//             <h3>No Results Found</h3>
+//             <p>You search did not return any results. Please try again.</p>
+//           </li>
+//         </ul>
+//       </div> */}
+
+//     </div>
+  // );
+// }
+
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Nav = (props) => {
+    return (
+        <nav className="main-nav">
+        <ul>
+          <li>Cats</li>
+          {/* <li><a href='#'>Dogs</a></li>
+          <li><a href='#'>Computers</a></li> */}
+        </ul>
+        </nav>
+    );
+  }
+  
+  export default Nav;
+  
+  
